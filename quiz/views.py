92c14a31c329c269f931378d8bae44d8f73ea5b8
KristinaKockum <kristina.kockum@alumni.hhs.se>
2016-09-08 13:28:14 +0200

    Lagt in testdata

--- quiz/views.py
+++ quiz/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+quizzes = [
+	{
+		"quiz_number": 1,
+		"name": "Klassiska böcker",
+		"description": "Hur bra kan du dina klassiker?"
+	},
+	{
+		"quiz_number": 2,
+		"name": "Största slagen",
+		"description": "Kan du dina lag?"
+	},
+	{
+		"quiz_number": 3,
+		"name": "Världens mest kända hackare",
+		"description": "Hackerhistoria är viktigt, kan du den?"
+	},
+]
+
 def startpage(request):
+	context={
+		"quizzes": quizzes,
+		}
 	return render(request, "quiz/start.html")
 
 def quiz(request):
