2c52a943155005118d30d549248d95fa1fc568a0
KristinaKockum <kristina.kockum@alumni.hhs.se>
2016-09-08 11:57:54 +0200

    Lagt till vyer

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+def startpage(request):
+	return render(request, "quiz/start.html")
 
-# Create your views here.
+def quiz(request):
+	return render(request, "quiz/quiz.html")
+
+def question(request):
+	return render(request, "quiz/question.html")
+
+def completed(request):
+	return render(request, "quiz/result.html")
+
+			
+
+#
