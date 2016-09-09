f41245d0c71d0d8c67da49059042203e4f5006cc
KristinaKockum <kristina.kockum@alumni.hhs.se>
2016-09-09 10:25:40 +0200

    Skapat admikonto django och lagt till quiz och questions i gränssnittet

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
 	url(r"^$", views.startpage, name="start_page"),
 	url(r"^quiz/([0-9])+/$", views.quiz,name="quiz_page"),
 	url(r"^quiz/([0-9])+/question/([0-9]+)/$", views.question, name="question_page"),
-	url(r"^quiz/([0-9])+/completed/$", views.completed, name="completed_page"),]
-	url(r'^admin/', include(admin.site.urls)),
+	url(r'^admin/', include(admin.site.urls)),]
