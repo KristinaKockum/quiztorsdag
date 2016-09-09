c7fecea4d460247c20bff1fc10a4c7b682ef8c32
KristinaKockum <kristina.kockum@alumni.hhs.se>
2016-09-09 11:52:54 +0200

    Fungerar till och med questions

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from django.conf.urls import include
 from django.conf.urls import url
 from quiz import views
+
+
 urlpatterns = [
-	url(r"^$", views.startpage, name="start_page"),
-	url(r"^quiz/([0-9])+/$", views.quiz,name="quiz_page"),
-	url(r"^quiz/([0-9])+/question/([0-9]+)/$", views.question, name="question_page"),
-	url(r'^admin/', include(admin.site.urls)),]
+	url(r"^$", views.start, name="start_page"),
+	url(r"^quiz/([0-9]+)/$", views.quiz, name="quiz_page"),
+	url(r"^quiz/([0-9]+)/question/([0-9]+)/$", views.question, name="question_page"),
+	url(r"^quiz/([0-9]+)/result/$", views.result, name="results_page"),
+	url(r'^admin/', include(admin.site.urls)),
+	]
